02f6e7c9842e1e88e0c83501637715e81c3a0fae
= <=>
2021-04-12 00:43:49 +0700

    GetX Stream

--- lib/controllers/infinite_controller.dart
+++ lib/controllers/infinite_controller.dart
@@ -3,9 +3,10 @@ import 'package:flutter_samples/model/post.dart';
 import 'package:flutter_samples/provider/post_getx_provider.dart';
 import 'package:get/get.dart';
 
+//GETX - Reactive or STREAM
 class InfiniteController extends GetxController {
-  List<Post> posts;
-  bool hasReachedMax = false;
+  var posts = [].obs;
+  var hasReachedMax = false.obs;
 
   ScrollController controller = ScrollController();
 
@@ -21,40 +22,40 @@ class InfiniteController extends GetxController {
   }
 
   void onFirstTime() {
-    if (posts != null) {
+    if (posts.value != null) {
       posts.clear();
-      posts = null;
-      update();
+      posts.value = null;
+      //update(['aVeryUniqueID']);
     }
     myPost();
   }
 
   void myPost() async {
     List<Post> _posts;
-    PostGetxProvider _postGetxProvider;
+    //PostGetxProvider _postGetxProvider;
 
-    if (posts == null) {
+    if (posts.value == null) {
       _posts = await Post.connectToApi(0, 10);
       //_posts = await _myPostClass.connectToApiTwo(0, 10);
 
       //jika belum ada datanya (null maka) state hasReachedMax=true dan posts=null;
       //selainnya maka ambil dan kembalikan dengan state isi data dan hasReachedMax=false;
       if (_posts == null) {
-        hasReachedMax = true;
-        posts = null;
+        hasReachedMax.value = true;
+        posts.value = null;
       } else {
-        posts = _posts;
-        hasReachedMax = false;
+        posts.value = _posts;
+        hasReachedMax.value = false;
       }
     } else {
       //ambil post berikutnya dengan parameter start berasal dari jumlah post sebelumnya
-      _posts = await Post.connectToApi(posts.length, 10);
+      _posts = await Post.connectToApi(posts.value.length, 10);
       //_posts = await _myPostClass.connectToApiTwo(posts.length, 10);
 
-      hasReachedMax = (_posts.isEmpty) ? true : false;
-      posts = (_posts.isEmpty) ? posts : posts + _posts;
+      hasReachedMax.value = (_posts.isEmpty) ? true : false;
+      posts.value = (_posts.isEmpty) ? posts.value : posts.value + _posts;
     }
-    update();
+    //update(['aVeryUniqueID']);
   }
 
   @override
@@ -78,3 +79,80 @@ class InfiniteController extends GetxController {
     super.onClose();
   }
 }
+
+//GETBUILDER or Not Reactive
+/* class InfiniteController extends GetxController {
+  List<Post> posts;
+  bool hasReachedMax = false;
+
+  ScrollController controller = ScrollController();
+
+  void onScroll() {
+    //ujung bawah screen
+    double maxScroll = controller.position.maxScrollExtent;
+    //posisi kini screen
+    double currentScroll = controller.position.pixels;
+
+    if (currentScroll == maxScroll) {
+      myPost();
+    }
+  }
+
+  void onFirstTime() {
+    if (posts != null) {
+      posts.clear();
+      posts = null;
+      update(['aVeryUniqueID']);
+    }
+    myPost();
+  }
+
+  void myPost() async {
+    List<Post> _posts;
+    //PostGetxProvider _postGetxProvider;
+
+    if (posts == null) {
+      _posts = await Post.connectToApi(0, 10);
+      //_posts = await _myPostClass.connectToApiTwo(0, 10);
+
+      //jika belum ada datanya (null maka) state hasReachedMax=true dan posts=null;
+      //selainnya maka ambil dan kembalikan dengan state isi data dan hasReachedMax=false;
+      if (_posts == null) {
+        hasReachedMax = true;
+        posts = null;
+      } else {
+        posts = _posts;
+        hasReachedMax = false;
+      }
+    } else {
+      //ambil post berikutnya dengan parameter start berasal dari jumlah post sebelumnya
+      _posts = await Post.connectToApi(posts.length, 10);
+      //_posts = await _myPostClass.connectToApiTwo(posts.length, 10);
+
+      hasReachedMax = (_posts.isEmpty) ? true : false;
+      posts = (_posts.isEmpty) ? posts : posts + _posts;
+    }
+    update(['aVeryUniqueID']);
+  }
+
+  @override
+  void onReady() {
+    print('ready');
+    super.onReady();
+  }
+
+  @override
+  void onInit() {
+    print('init');
+    controller.addListener(onScroll);
+    //myPost();
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    print('onclose');
+    controller.dispose();
+    super.onClose();
+  }
+} */
--- lib/infinite_loading_getx_screen.dart
+++ lib/infinite_loading_getx_screen.dart
@@ -6,6 +6,8 @@ import './ui/post_item.dart';
 
 //main source : https://pub.dev/packages/get/example
 //source : https://github.com/jonataslaw/getx/blob/master/documentation/en_US/route_management.md#navigation-with-named-routes
+
+//GETX = STREAM , STREAM is POWERFULL BUT USE MORE MEMORY
 class InfiniteLoadingGetxScreen extends GetView<InfiniteController> {
   @override
   Widget build(BuildContext context) {
@@ -16,7 +18,69 @@ class InfiniteLoadingGetxScreen extends GetView<InfiniteController> {
       appBar: AppBar(
         title: Text("Infinite Loading with GetX"),
       ),
+      body: GetX<InfiniteController>(
+        init: InfiniteController(),
+        builder: (value) {
+          if (value.hasReachedMax.value) {
+            WidgetsBinding.instance.addPostFrameCallback((_) => Get.snackbar(
+                  "Hi",
+                  "Record has reached maximum...",
+                  duration: Duration(seconds: 3),
+                  snackPosition: SnackPosition.BOTTOM,
+                ));
+          }
+          return Container(
+            margin: EdgeInsets.only(left: 20, right: 20),
+            child: (controller.posts.value == null)
+                ? Center(
+                    child: SizedBox(
+                      width: 130,
+                      height: 130,
+                      child: FlareActor(
+                        "assets/loading_success_error.flr",
+                        animation: "loading",
+                      ),
+                    ),
+                  )
+                : ListView.builder(
+                    //controller men-trigger cubit bloc
+                    controller: value.controller,
+                    itemCount: (value.hasReachedMax.value)
+                        ? value.posts.value.length
+                        : value.posts.value.length + 1,
+                    itemBuilder: (context, index) =>
+                        (index < value.posts.value.length)
+                            ? PostItem(value.posts.value[index])
+                            : Container(
+                                child: Center(
+                                  child: SizedBox(
+                                    width: 30,
+                                    height: 30,
+                                    child: CircularProgressIndicator(),
+                                  ),
+                                ),
+                              ),
+                  ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+//GETBUILDER - ALSO POWERFULL BECOUSE USE LESS MEMORY BUT NOT STREAMER (DATANYA TIDAK MENGALIR)
+/* class InfiniteLoadingGetxScreen extends GetView<InfiniteController> {
+  @override
+  Widget build(BuildContext context) {
+    controller.onFirstTime();
+
+    return Scaffold(
+      //key: _scaffoldKey,
+      appBar: AppBar(
+        title: Text("Infinite Loading with GetX"),
+      ),
       body: GetBuilder<InfiniteController>(
+        id: 'aVeryUniqueID', // here
         init: InfiniteController(),
         builder: (value) {
           if (controller.hasReachedMax) {
@@ -64,7 +128,7 @@ class InfiniteLoadingGetxScreen extends GetView<InfiniteController> {
       ),
     );
   }
-}
+} */
 
 /*
 class InfiniteLoadingGetxScreen extends StatelessWidget {
